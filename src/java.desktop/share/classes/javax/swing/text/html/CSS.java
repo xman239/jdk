4b05b937f77aeb4e071375c98085f69602bc6ca6
ScientificWare <info@scientificware.com>
2022-09-14 05:47:11 +0200

    Author tag, Modifier, Returned value.
    Removes the last added author tag.
    Returns the Color instance from the map if there's one.
    Renames colorNamed Object to colorNames.
    Adds final modifier to colorNames.

--- src/java.desktop/share/classes/javax/swing/text/html/CSS.java
+++ src/java.desktop/share/classes/javax/swing/text/html/CSS.java
@@ -130,7 +130,6 @@ import javax.swing.text.View;
  *
  * @author  Timothy Prinzing
  * @author  Scott Violet
- * @author  Guy Abossolo Foh - ScientificWare
  * @see StyleSheet
  */
 @SuppressWarnings("serial") // Same-version serialization only
@@ -1407,16 +1406,16 @@ public class CSS implements Serializable {
         } else if (str.charAt(0) == '#') {
             return hexToColor(str);
         } else {
-            Color color = colorNamed.get(strlc);
+            Color color = colorNames.get(strlc);
             if (color != null) {
-                return new Color(color.getRGB(), true);
+                return color;
             }
             // sometimes get specified without leading #
             return hexToColor(str);
         }
     }
 
-    private static Map<String, Color> colorNamed =
+    private static final Map<String, Color> colorNames =
         Map.ofEntries(
             Map.entry("aliceblue", new Color(240, 248, 255)),
             Map.entry("antiquewhite", new Color(250, 235, 215)),
